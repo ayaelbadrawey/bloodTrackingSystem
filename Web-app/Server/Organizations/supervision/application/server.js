c5ab6d310fdcab1daa1e6289e9b86471dd47f90a
ayaelbadrawey <ubuntu@ubun2004.linuxvmimages.local>
2021-07-16 12:39:00 -0400

    supervision server

--- Web-app/Server/Organizations/supervision/application/server.js
+++ Web-app/Server/Organizations/supervision/application/server.js
@@ -17,7 +17,7 @@ const morgan = require('morgan');
 const app = express();
 const httpServer = http.createServer(app);
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = 5002 ;
 
 app.use(morgan('combined'));
 app.use(cors());
@@ -33,6 +33,9 @@ app.set('port', PORT);
 app.route('/query/bag').get(async(req, res, next)=>{
     const gateway = new Gateway();
     try {
+
+        const id1 = req.query.id;
+
         // Specify userName for network access
         // const userName = 'isabella.issuer@magnetocorp.com';
         const userName = 'Manager@org1.example.com';
@@ -58,7 +61,7 @@ app.route('/query/bag').get(async(req, res, next)=>{
         const contract = await network.getContract('bloodcontract');
 
         console.log('--------------------Query Blood Bag--------------------');
-        const qBagResponse1 = await contract.evaluateTransaction('queryBloodBag', 'BD500:AB+');
+        const qBagResponse1 = await contract.evaluateTransaction('queryBloodBag', id1);
         console.log(`${qBagResponse1.toString()}`);
         console.log('Transaction complete.');
 
@@ -137,6 +140,9 @@ app.route('/query/all/bags').get(async(req, res, next)=>{
 app.route('/get/history').get(async(req, res, next)=>{
     const gateway = new Gateway();
     try {
+
+        const id1 = req.query.id;
+
         // Specify userName for network access
         // const userName = 'isabella.issuer@magnetocorp.com';
         const userName = 'Manager@org1.example.com';
@@ -162,7 +168,7 @@ app.route('/get/history').get(async(req, res, next)=>{
         const contract = await network.getContract('bloodcontract');
 
         console.log('--------------------History of Blood Bag--------------------');
-        const historyResponse = await contract.evaluateTransaction('getHistoryForBloodBag', 'BD244:O-');
+        const historyResponse = await contract.evaluateTransaction('getHistoryForBloodBag', id1);
         console.log(`${historyResponse.toString()}`);
         console.log('Transaction complete.');
 
@@ -186,113 +192,12 @@ app.route('/get/history').get(async(req, res, next)=>{
     
 });
 
-app.route('/query/bloodbank').get(async(req, res, next)=>{
-    const gateway = new Gateway();
-    try {
-        // Specify userName for network access
-        // const userName = 'isabella.issuer@magnetocorp.com';
-        const userName = 'Manager@org1.example.com';
-
-        // Load connection profile; will be used to locate a gateway
-        let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/networkConnection.yaml', 'utf8'));
-
-        // Set connection options; identity and wallet
-        let connectionOptions = {
-            identity: userName,
-            wallet: wallet,
-            discovery: { enabled:false, asLocalhost: true }
-        };
-
-        // Connect to gateway using application specified parameters
-        console.log('Connect to Fabric gateway.');
-        await gateway.connect(connectionProfile, connectionOptions);
-
-        // Access PaperNet network
-        console.log('Use network channel: mychannel.');
-
-        const network = await gateway.getNetwork('mychannel');
-        const contract = await network.getContract('bloodcontract');
-
-        console.log('--------------------Query Blood Bags of Blood Bank--------------------');
-        const qbloodbankResponse = await contract.evaluateTransaction('queryBloodBankOwner', 'BB101');
-        console.log(`${qbloodbankResponse.toString()}`);
-        console.log('Transaction complete.');
-
-        const output = qbloodbankResponse.toString()
-
-        res.status(200).json({
-            output
-        });
-    }
-     catch (error) {
-
-        console.log(`Error processing transaction. ${error}`);
-        console.log(error.stack);
-
-}   finally {
-        // Disconnect from the gateway
-        console.log('Disconnect from Fabric gateway.');
-        gateway.disconnect();
-
-}
-    
-});
-
-app.route('/query/hospital').get(async(req, res, next)=>{
-    const gateway = new Gateway();
-    try {
-        // Specify userName for network access
-        // const userName = 'isabella.issuer@magnetocorp.com';
-        const userName = 'Manager@org1.example.com';
-
-        // Load connection profile; will be used to locate a gateway
-        let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/networkConnection.yaml', 'utf8'));
-
-        // Set connection options; identity and wallet
-        let connectionOptions = {
-            identity: userName,
-            wallet: wallet,
-            discovery: { enabled:false, asLocalhost: true }
-        };
-
-        // Connect to gateway using application specified parameters
-        console.log('Connect to Fabric gateway.');
-        await gateway.connect(connectionProfile, connectionOptions);
-
-        // Access PaperNet network
-        console.log('Use network channel: mychannel.');
-
-        const network = await gateway.getNetwork('mychannel');
-        const contract = await network.getContract('bloodcontract');
-
-        console.log('--------------------Query Blood Bags of Hospital--------------------');
-        const qhospitalResponse = await contract.evaluateTransaction('queryHospitalOwner', 'H101');
-        console.log(`${qhospitalResponse.toString()}`);
-        console.log('Transaction complete.');
-
-        const output = qhospitalResponse.toString()
-
-        res.status(200).json({
-            output
-        });
-    }
-     catch (error) {
-
-        console.log(`Error processing transaction. ${error}`);
-        console.log(error.stack);
-
-}   finally {
-        // Disconnect from the gateway
-        console.log('Disconnect from Fabric gateway.');
-        gateway.disconnect();
-
-}
-    
-});
-
 app.route('/query/process').get(async(req, res, next)=>{
     const gateway = new Gateway();
     try {
+
+        const pid1 = req.query.pid;
+
         // Specify userName for network access
         // const userName = 'isabella.issuer@magnetocorp.com';
         const userName = 'Manager@org1.example.com';
@@ -318,7 +223,7 @@ app.route('/query/process').get(async(req, res, next)=>{
         const contract = await network.getContract('bloodcontract');
 
         console.log('--------------------Query Processes--------------------');
-        const qprocessResponse1 = await contract.evaluateTransaction('queryProcess', 'P101');
+        const qprocessResponse1 = await contract.evaluateTransaction('queryProcess', pid1);
         console.log(`${qprocessResponse1.toString()}`);
         console.log('Transaction complete.');
 
