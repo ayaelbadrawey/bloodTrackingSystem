8592a1c51b95617e1d7087aacb1f2199ec4fabeb
ayaelbadrawey <ubuntu@ubun2004.linuxvmimages.local>
2021-07-19 10:09:13 -0400

    adding bloodbank pages

--- Web-app/Client/src/components/contents/bloodbank/maincontent.js
+++ Web-app/Client/src/components/contents/bloodbank/maincontent.js
@@ -1,16 +1,27 @@
 import {useHistory} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
 import BloodBankHeader from '../../headers/bloodbank'
+import HomeContent from '../home'
+import BloodBankLogin from './login'
+import BloodBankDonation from './donation'
+import BloodBankOptions from './options'
+
 
 function BloodBankContent() {
   let history = useHistory();
     return (
       <div>
-        <BloodBankHeader />
-        <h1>Blood bank Content</h1>
-        <button onClick={()=>{history.push("/home");}}>go to home</button>
-        <button onClick={()=>{history.push("/hospital");}}>go to hospital</button>
-        <button onClick={()=>{history.push("/supervision");}}>go to supervision</button>
-        <button onClick={()=>{history.push("/user");}}>go to user</button>
+        <Router>
+        <BloodBankHeader /> 
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+        <button onClick={()=>{history.push("/options");}}>go to options</button>
+        <button onClick={()=>{history.push("/login");}}>login</button>
+        <button onClick={()=>{history.push("/donation");}}>New Donation</button>
+        <button onClick={()=>{history.push("/bloodBagRetrieve");}}>Blood Bags Retrieve</button>
+        <button onClick={()=>{history.push("/bloodBagSendBag");}}>send Blood bag</button>
+       </Router>
+      
       </div>
     );
   }
