167c9920bb03e4bf72c34dc3b1af7b336f53a147
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2021-07-20 02:58:26 -0400

    database

---
+++ Web-app/Server/db.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const {createPool} = require('mysql');
+
+const db = createPool({
+    host:"localhost",
+    user:"root",
+    password: "root12345",
+    database:"info",
+    connectionLimit: 10
+})
+app.get("/api/get", (req,res) => {
+    const sql = "SELECT * FROM bloodbank WHERE bbId='BB100';";
+    db.query(sql,(err, result) => {
+        console.log(result);
+        res.send(result);
+        
+    });
+});
+app.get("/api/get/bloodbank", (req,res) => {
+    const email = req.query.email;
+    const sql = "SELECT * FROM bloodbank WHERE bbEmail='"+email+"'";
+    db.query(sql,(err, result) => {
+        console.log(result);
+        res.send(result);
+        
+    });
+});
+app.listen(3000, ()=> {
+    console.log("running on port 3000");
+});
