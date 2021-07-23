15a8dbe56fafa98f2f637af997fdbb81e7105012
ayaelbadrawey <ubuntu@ubun2004.linuxvmimages.local>
2021-07-23 07:21:23 -0400

    modifications in db server

--- Web-app/Server/db.js
+++ Web-app/Server/db.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {createPool} = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const db = createPool({
     host:"localhost",
@@ -35,6 +40,45 @@ app.get("/api/get/bloodbank", (req,res) => {
         
     });
 });
+app.post("/insert/patient", (req,res) => {
+    const ID = req.body.ID;
+    const Email = req.body.Email;
+    const sql = "INSERT INTO patient (pID, pEmail) VALUES (?,?)";;
+    db.query(sql, [ID, Email], (err, result) => {
+        console.log(result);
+        console.log(err)
+        res.send(result);
+    });
+});
+
+app.get("/check/patient", (req,res) => {
+    const email = req.query.email;
+    const sql = "SELECT pID FROM patient WHERE pEmail='"+email+"'";
+    db.query(sql,(err, result) => {
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.post("/insert/donor", (req,res) => {
+    const ID = req.body.ID;
+    const Email = req.body.Email;
+    const sql = "INSERT INTO donor (dID, dEmail) VALUES (?,?)";;
+    db.query(sql, [ID, Email], (err, result) => {
+        console.log(result);
+        console.log(err)
+        res.send(result);
+    });
+});
+
+app.get("/check/donor", (req,res) => {
+    const email = req.query.email;
+    const sql = "SELECT dID FROM donor WHERE dEmail='"+email+"'";
+    db.query(sql,(err, result) => {
+        console.log(result);
+        res.send(result);
+    });
+});
 app.get("/api/write/file", (req,res) => {
     const file = req.query.file;
     const writeJsonFile = require('write-json-file');
@@ -43,6 +87,6 @@ app.get("/api/write/file", (req,res) => {
     })();
     res.send('This is after the write call');
 });
-app.listen(4000, ()=> {
-    console.log("running on port 4000");
+app.listen(5004, ()=> {
+    console.log("running on port 5004");
 });
