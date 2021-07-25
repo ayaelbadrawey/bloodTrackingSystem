680f7124f36ccaee47323fbf66d6560652b9fc91
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2021-07-25 01:49:16 -0400

    instantiate

--- Web-app/Server/Organizations/bloodbank/application/instantiate.js
+++ Web-app/Server/Organizations/bloodbank/application/instantiate.js
@@ -47,6 +47,30 @@ async function main() {
 
         const instantiateRecieve = await contract.submitTransaction('instantiateRecieve');
         console.log(' instantiate recieve complete.');
+
+        //test cases
+
+        //add blood "BD1500:B-" "BB100" "D100" state=>ready
+        const test1 = await contract.submitTransaction(' createBloodBag','BD1500','B-','25/7/2021','20/10/2021','SAFE','D100','4','25/7/2021 12:00:00','BB100');
+        console.log("test1",test1)
+
+        //add blood "BD1501:AB-" "BB100" "D101" state=>under transportation
+        const test2 = await contract.submitTransaction(' createBloodBag','BD1501','AB-','7/7/2021','2/10/2021','SAFE','D101','3','7/7/2021 12:00:00','BB100');
+        console.log("test2",test2)
+
+        const test3 = await contract.submitTransaction(' underTransportBloodDIN','BD1501:AB-','7/30/2021 06:30:00');
+        console.log("test3",test3)
+
+        //add blood "BD1502:O+" "BB101" "D101" "H100" state=>under delivered
+        const test4 = await contract.submitTransaction(' createBloodBag','BD1502','O+','1/7/2021','30/9/2021','SAFE','D101','5','1/7/2021 12:00:00','BB101');
+        console.log("test4",test4)
+
+        const test5 = await contract.submitTransaction(' underTransportBloodDIN','BD1502:O+','3/7/2021 06:30:00');
+        console.log("test5",test5)
+
+        const test6 = await contract.submitTransaction(' delieveredBloodDIN','BD1502:O+','3/7/2021 09:40:00');
+        console.log("test6",test6)
+
       
 
     } catch (error) {
